3ea454ff0a6e113dab7a54c1712689608ad64076
markhabaerzhan <markhaba.erzhankyzy@narxoz.kz>
2026-05-09 18:07:24 +0500

    add GuildMediator interface

--- src/com/narxoz/rpg/guild/GuildMediator.java
+++ src/com/narxoz/rpg/guild/GuildMediator.java
@@ -1,11 +1,7 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Central communication contract for guild colleagues.
- */
 public interface GuildMediator {
 
     void register(GuildMember member);
-
     void dispatch(String topic, GuildMember from, String payload);
 }
